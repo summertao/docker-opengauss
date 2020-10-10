d6ec667b84e3ac812d2596efc7032473d41b9279
summertao <summertao@outlook.com>
2020-10-10 16:10:40 +0800

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
-# 1. Build Image
+# 一、OpenGauss Server编译流程
+## 1. 下载openGauss源码
+可以从开源社区下载openGauss-server源码。下载解压后重命名为openGauss-server。
+可以通过[此处](https://opengauss.obs.cn-south-1.myhuaweicloud.com/1.0.0/openGauss-third_party_binarylibs.tar.gz)获取编译好的binarylibs。下载后解压缩并重命名为binarylibs。
+
+## 2. 配置环境变量
+```console
+export CODE_BASE=/home/omm/software/openGauss-server
+export BINARYLIBS=/home/omm/software/binarylibs
+export GAUSSHOME=/home/omm/software/openGauss-server/dest
+export GCC_PATH=$BINARYLIBS/buildtools/centos7.6_x86_64/gcc8.2/
+export CC=$GCC_PATH/gcc/bin/gcc
+export CXX=$GCC_PATH/gcc/bin/g++
+export LD_LIBRARY_PATH=$GAUSSHOME/lib:$GCC_PATH/gcc/lib64:$GCC_PATH/isl/lib:$GCC_PATH/mpc/lib/:$GCC_PATH/mpfr/lib/:$GCC_PATH/gmp/lib/:$LD_LIBRARY_PATH
+export PATH=$GAUSSHOME/bin:$GCC_PATH/gcc/bin:$PATH
+```
+
+## 3. 执行build
+```console
+$ ./build.sh -3rd $BINARYLIBS
+
+```
+
+编译完的文件在$GAUSSHOME文件夹中，默认为Release版本。
+
+## 4. 编译完成后打包
+
+```console
+$ cd $GAUSSHOME
+$ tar -jcvf ../openGauss-1.0.0-CentOS-64bit.tar.bz2 ./
+```
+
+# 二、OpenGauss Docker部署流程
+
+## 1. Build Image
 
 ```console
 $ ./buildDockerImage.sh
 ```
 
-# 2. Create Master/Slave Containers
+## 2. Create Master/Slave Containers
 
 ```console
 $ ./create_master_slave_op.sh
 ```
 
-# 3. Inspect Sync Info in Master/Slave
+## 3. Inspect Sync Info in Master/Slave
 
 ```console
 $ docker exec -it $master_container_id/$slave_container_id bash
@@ -19,7 +53,7 @@ $ gs_ctl query -D $PGDATA/
 $ 
 ```
 
-# 4. Connect to Master DB
+## 4. Connect to Master DB
 
 ```console
 $ docker exec -it $master_container_id/$slave_container_id bash
@@ -28,10 +62,10 @@ $ gsql -p 5432 # slave1端口为6432
 $ 
 ```
 
-# 5. Set up DB
+## 5. Set up DB
 > ***进一步初始化数据库和添加应用用户请参考：***[https://www.modb.pro/db/27572](https://www.modb.pro/db/27572)
 
-# 持久化存储数据
+## 持久化存储数据
 容器一旦被删除，容器内的所有数据和配置也均会丢失，而从镜像重新运行一个容器的话，则所有数据又都是呈现在初始化状态，因此对于数据库容器来说，为了防止因为容器的消亡或者损坏导致的数据丢失，需要进行持久化存储数据的操作。通过在`docker run`的时候指定`-v`参数来实现。比如以下命令将会指定将openGauss的所有数据文件存储在宿主机的summertao/opengauss下。
 
 ```console
--- create_single_node_op.sh
+++ create_single_node_op.sh
@@ -12,15 +12,8 @@ echo "OpenGauss Database Network Created."
 # Parameters
 GS_PASSWORD="omm@Password123"
 MASTER_IP="172.18.0.101"
-SLAVE_1_IP="172.18.0.102"
-SLAVE_2_IP="172.18.0.103"
-SLAVE_3_IP="172.18.0.104"
 
 MASTER_HOST_PORT="5432"
-MASTER_LOCAL_PORT="5434"
-
-SLAVE_1_HOST_PORT="6432"
-SLAVE_1_LOCAL_PORT="6434"
 
 PGAPPNAME="opengauss_master"
 docker run  --network opengaussnetwork --ip $MASTER_IP --privileged=true \
@@ -30,8 +23,8 @@ docker run  --network opengaussnetwork --ip $MASTER_IP --privileged=true \
 -e OG_SUBNET=$OG_SUBNET \
 -e GS_PASSWORD=$GS_PASSWORD \
 -e NODE_NAME=$PGAPPNAME \
--e REPL_CONN_INFO="replconninfo1 = 'localhost=$MASTER_IP localport=$MASTER_LOCAL_PORT localservice=$MASTER_HOST_PORT remotehost=$SLAVE_1_IP remoteport=$SLAVE_1_LOCAL_PORT remoteservice=$SLAVE_1_HOST_PORT'\n" \
-summertao/opengauss:1.0.0 -M primary \
+-v /Users/abc/Documents/software/docker-opengauss/data:/var/lib/opengauss/data \
+summertao/opengauss:1.0.0 \
 || {
   echo ""
   echo "ERROR: OpenGauss Database Master Docker Container was NOT successfully created."
@@ -39,24 +32,24 @@ summertao/opengauss:1.0.0 -M primary \
 }
 echo "OpenGauss Database Master Docker Container created."
 
-sleep 30s
+# sleep 30s
 
-PGAPPNAME="opengauss_slave1"
-docker run --network opengaussnetwork --ip $SLAVE_1_IP --privileged=true \
---name $PGAPPNAME -h $PGAPPNAME -p $SLAVE_1_HOST_PORT:$SLAVE_1_HOST_PORT -d \
--e PGAPPNAME=$PGAPPNAME \
--e GS_PORT=$SLAVE_1_HOST_PORT \
--e OG_SUBNET=$OG_SUBNET \
--e GS_PASSWORD=$GS_PASSWORD \
--e NODE_NAME=$PGAPPNAME \
--e REPL_CONN_INFO="replconninfo1 = 'localhost=$SLAVE_1_IP localport=$SLAVE_1_LOCAL_PORT localservice=$SLAVE_1_HOST_PORT remotehost=$MASTER_IP remoteport=$MASTER_LOCAL_PORT remoteservice=$MASTER_HOST_PORT'\n" \
-summertao/opengauss:1.0.0 -M standby \
-|| {
-  echo ""
-  echo "ERROR: OpenGauss Database Slave1 Docker Container was NOT successfully created."
-  exit 1
-}
-echo "OpenGauss Database Slave1 Docker Container created."
+# PGAPPNAME="opengauss_slave1"
+# docker run --network opengaussnetwork --ip $SLAVE_1_IP --privileged=true \
+# --name $PGAPPNAME -h $PGAPPNAME -p $SLAVE_1_HOST_PORT:$SLAVE_1_HOST_PORT -d \
+# -e PGAPPNAME=$PGAPPNAME \
+# -e GS_PORT=$SLAVE_1_HOST_PORT \
+# -e OG_SUBNET=$OG_SUBNET \
+# -e GS_PASSWORD=$GS_PASSWORD \
+# -e NODE_NAME=$PGAPPNAME \
+# -e REPL_CONN_INFO="replconninfo1 = 'localhost=$SLAVE_1_IP localport=$SLAVE_1_LOCAL_PORT localservice=$SLAVE_1_HOST_PORT remotehost=$MASTER_IP remoteport=$MASTER_LOCAL_PORT remoteservice=$MASTER_HOST_PORT'\n" \
+# summertao/opengauss:1.0.0 -M standby \
+# || {
+#   echo ""
+#   echo "ERROR: OpenGauss Database Slave1 Docker Container was NOT successfully created."
+#   exit 1
+# }
+# echo "OpenGauss Database Slave1 Docker Container created."
 
 # docker run  --entrypoint "/bin/bash" --network opengaussnetwork --ip $SLAVE_1_IP --privileged=true \
 # --name opengauss_slave1 -h opengauss_slave1 -p $SLAVE_1_HOST_PORT:$SLAVE_1_HOST_PORT -d \
